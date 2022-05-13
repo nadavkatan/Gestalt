cc195dd775cc4f11e99bf47d56e7f4dd4333111f
nadavkatan <nadavkatan110@gmail.com>
2022-05-13 15:36:49 +0200

    Updating id's

--- app.js
+++ app.js
@@ -13,7 +13,7 @@ frame.on(
     // array to store the coordinates of the line drawn by the user
     let lineCords = [];
     //track the levels progress
-    let level = 12;
+    let level = 1;
     // initialize the var that contains the element id's of the elements selected by the user
     let selectedPoints;
     //For level 12
@@ -26,22 +26,22 @@ frame.on(
         numAnswersSubmitted: 10,
         answers:[
         {
-          answer: [106, 108, 110, 112, 114, 116, 118, 120, 122, 124, 126, 128, 130, 132, 134, 136, 138, 140, 142, 144, 146, 148, 150, 152],
+          answer: [112, 114, 116, 118, 120, 122, 124, 126, 128, 130, 132, 134, 136, 138, 140, 142, 144, 146, 148, 150, 152, 154, 156, 158],
           timesChosen: 10,
           percentage: 100,
           answerClass: 'proximity',
           principle: 'Proximity' 
         },
         {
-          answer: [154, 156, 158, 160, 162, 164, 166, 168, 170, 172, 174, 176, 178, 180, 182, 184, 186, 188, 190, 192, 194, 196, 198, 200],
+          answer: [160, 162, 164, 166, 168, 170, 172, 174, 176, 178, 180, 182, 184, 186, 188, 190, 192, 194, 196, 198, 200, 202, 204, 206],
           timesChosen: 10,
           percentage: 100,
           answerClass: 'proximity',
           principle: 'Proximity' 
         },
         {
-          answer: [106, 108, 110, 112, 114, 116, 118, 120, 122, 124, 126, 128, 130, 132, 134, 136, 138, 140, 142, 144, 146, 148, 150, 152,
-                  154, 156, 158, 160, 162, 164, 166, 168, 170, 172, 174, 176, 178, 180, 182, 184, 186, 188, 190, 192, 194, 196, 198, 200],
+          answer: [112, 114, 116, 118, 120, 122, 124, 126, 128, 130, 132, 134, 136, 138, 140, 142, 144, 146, 148, 150, 152, 154, 156, 158, 
+            160, 162, 164, 166, 168, 170, 172, 174, 176, 178, 180, 182, 184, 186, 188, 190, 192, 194, 196, 198, 200, 202, 204, 206],
           timesChosen: 0,
           percentage: 0,
           answerClass: undefined,
@@ -56,22 +56,22 @@ frame.on(
       numAnswersSubmitted: 10,
       answers: [
         {
-          answer: [202, 204, 206, 208, 210, 212, 214, 216, 218, 220, 222, 224, 226, 228, 230, 232],
+          answer:  [208, 210, 212, 214, 216, 218, 220, 222, 224, 226, 228, 230, 232, 234, 236, 238],
           timesChosen: 8,
           percentage: 80,
           answerClass: 'similarity',
           principle: 'Similarity'
         },
         { 
-          answer: [234, 236, 238, 240, 242, 244, 246, 248, 250, 252, 254, 256, 258, 260, 262, 264],
+          answer: [240, 242, 244, 246, 248, 250, 252, 254, 256, 258, 260, 262, 264, 266, 268, 270],
           timesChosen: 8,
           percentage: 80,
           answerClass: 'similarity',
           principle: 'Similarity'
         },
         {
-          answer: [202, 204, 206, 208, 210, 212, 214, 216, 218, 220, 222, 224, 226, 228, 230, 232,
-            234, 236, 238, 240, 242, 244, 246, 248, 250, 252, 254, 256, 258, 260, 262, 264],
+          answer: [208, 210, 212, 214, 216, 218, 220, 222, 224, 226, 228, 230, 232, 234, 236, 238,
+             240, 242, 244, 246, 248, 250, 252, 254, 256, 258, 260, 262, 264, 266, 268, 270],
             timesChosen: 2,
             percentage: 20,
             answerClass: undefined,
@@ -85,22 +85,22 @@ frame.on(
       numAnswersSubmitted: 10,
       answers: [
         {
-          answer: [298, 300, 302, 304, 306, 308, 310, 312, 314, 316, 318, 320, 322, 324, 326, 328],
+          answer: [304, 306, 308, 310, 312, 314, 316, 318, 320, 322, 324, 326, 328, 330, 332, 334],
           timesChosen: 8,
           percentage: 80,
           answerClass: 'similarity',
           principle: 'Similarity'
         },
         { 
-          answer: [266, 268, 270, 272, 274, 276, 278, 280, 282, 284, 286, 288, 290, 292, 294, 296],
+          answer: [272, 274, 276, 278, 280, 282, 284, 286, 288, 290, 292, 294, 296, 298, 300, 302],
           timesChosen: 8,
           percentage: 80,
           answerClass: 'similarity',
           principle: 'Similarity'
         },
         {
-          answer: [266, 268, 270, 272, 274, 276, 278, 280, 282, 284, 286, 288, 290, 292, 294, 296,
-            298, 300, 302, 304, 306, 308, 310, 312, 314, 316, 318, 320, 322, 324, 326, 328],
+          answer: [272, 274, 276, 278, 280, 282, 284, 286, 288, 290, 292, 294, 296, 298, 300, 302,
+             304, 306, 308, 310, 312, 314, 316, 318, 320, 322, 324, 326, 328, 330, 332, 334],
             timesChosen: 2,
             percentage: 20,
             answerClass: undefined,
@@ -114,35 +114,35 @@ frame.on(
       numAnswersSubmitted: 10,
       answers: [
         {
-          answer: [330,332,334,336,338,340,342, 358],
+          answer: [336, 338, 340, 342, 344, 346, 348, 364],
           timesChosen: 8,
           percentage: 80,
           answerClass: 'continuity',
           principle: 'Continuity'
         },
         { 
-          answer: [344,346,348,350,352,354,356,358,360,362,364,366,368,370,372],
+          answer: [350, 352, 354, 356, 358, 360, 362, 364, 366, 368, 370, 372, 374, 376, 378],
           timesChosen: 8,
           percentage: 80,
           answerClass: 'similarity',
           principle: 'Similarity'
         },
         {
-          answer: [330,332,334,336,338,340,342,344,346,348,350,352,354,356,358,360,362,364,366,368,370,372],
+          answer: [336, 338, 340, 342, 344, 346, 348, 350, 352, 354, 356, 358, 360, 362, 364, 366, 368, 370, 372, 374, 376, 378],
             timesChosen: 1,
             percentage: 10,
             answerClass: undefined,
             principle: 'Proximity'
         },
         {
-          answer: [330,332,334,336,338,340,342,358,360,362,364,366,368,370,372],
+          answer: [336, 338, 340, 342, 344, 346, 348, 364, 366, 368, 370, 372, 374, 376, 378],
           timesChosen: 1,
           percentage: 10,
           answerClass: 'lShape',
           principle: 'Continuity'
       },
       {
-        answer: [330,332,334,336,338,340,342,344,346,348,350,352,354,356, 358],
+        answer: [336, 338, 340, 342, 344, 346, 348, 350, 352, 354, 356, 358, 360, 362, 364],
         timesChosen: 1,
         percentage: 10,
         answerClass: 'lShape',
@@ -157,49 +157,50 @@ frame.on(
       numAnswersSubmitted: 10,
       answers: [
         {
-          answer: [380, 382, 384, 386, 388, 390],
+          answer: [386, 388, 390, 392, 394, 396],
           timesChosen: 8,
           percentage: 80,
           answerClass: 'continuity',
           principle: 'Continuity'
         },
         { 
-          answer: [390, 392, 394, 396, 398, 400],
+          answer: [396, 398, 400, 402, 404, 406],
           timesChosen: 8,
           percentage: 80,
           answerClass: 'continuity',
           principle: 'Continuity'
         },
         {
-          answer: [400, 402, 404, 406, 408, 410],
+          answer: [406, 408, 410, 412, 414, 416],
             timesChosen: 9,
             percentage: 90,
             answerClass: 'continuity',
             principle: 'Continuity'
         },
         {
-          answer: [410, 412, 414, 416, 418, 420],
+          answer: [416, 418, 420, 422, 424, 426],
           timesChosen: 9,
           percentage: 90,
           answerClass: 'continuity',
           principle: 'Continuity'
         },
         {
-          answer: [420, 422, 424, 426, 428, 430],
+          answer:  [426, 428, 430, 432, 434, 436],
           timesChosen: 9,
           percentage: 90,
           answerClass: 'continuity',
           principle: 'Continuity'
         },
         {
-          answer: [430, 432, 434, 436, 438, 440],
+          answer: [436, 438, 440, 442, 444, 446],
           timesChosen: 9,
           percentage: 90,
           answerClass: 'continuity',
           principle: 'Continuity'
         },
         {
-          answer: [380, 382, 384, 386, 388, 390, 392, 394, 396, 398, 400, 402, 404, 406, 408, 410, 412, 414, 416, 418, 420, 422, 424, 426, 428, 430, 432, 434, 436, 438, 440],
+          answer: [386, 388, 390, 392, 394, 396, 398, 400, 402, 404, 406, 408, 410, 412, 414, 416, 418, 420, 
+            422, 424, 426, 428, 430, 432, 434, 436, 438, 440, 442, 444, 446],
             timesChosen: 1,
             percentage: 10,
             answerClass: 'hierarchy-continuity',
@@ -213,28 +214,28 @@ frame.on(
       numAnswersSubmitted: 10,
       answers: [
         {
-          answer: [480, 482],
+          answer: [486, 488],
           timesChosen: 10,
           percentage: 100,
           answerClass: 'common-region',
           principle: 'Common region'
         },
         { 
-          answer: [516, 518],
+          answer: [522, 524],
           timesChosen: 10,
           percentage: 100,
           answerClass: 'common-region',
           principle: 'Common region'
         },
         {
-          answer: [552, 554],
+          answer: [558, 560],
           timesChosen: 10,
           percentage: 100,
           answerClass: 'common-region',
           principle: 'Common region'
         },
         {
-          answer: [588, 590],
+          answer: [594, 596],
           timesChosen: 10,
           percentage: 100,
           answerClass: 'common-region',
@@ -248,21 +249,21 @@ frame.on(
       numAnswersSubmitted: 10,
       answers: [
         {
-          answer: [598, 637],
+          answer: [614, 643],
           timesChosen: 10,
           percentage: 100,
           answerClass: 'parallelism',
           principle: 'Parallelism'
         },
         { 
-          answer: [676, 715],
+          answer: [682, 721],
           timesChosen: 10,
           percentage: 100,
           answerClass: 'parallelism',
           principle: 'Parallelism'
         },
         {
-          answer: [754, 793],
+          answer: [760, 799],
           timesChosen: 10,
           percentage: 100,
           answerClass: 'parallelism',
@@ -275,21 +276,21 @@ frame.on(
       numAnswersSubmitted: 10,
       answers: [
         {
-          answer: [838, 877],
+          answer: [842, 883],
           timesChosen: 10,
           percentage: 100,
           answerClass: 'symmetry',
           principle: 'Symmetry'
         },
         { 
-          answer: [916, 955],
+          answer: [922, 961],
           timesChosen: 10,
           percentage: 100,
           answerClass: 'symmetry',
           principle: 'Symmetry'
         },
         {
-          answer: [994, 1033],
+          answer: [1000, 1039],
           timesChosen: 10,
           percentage: 100,
           answerClass: 'symmetry',
@@ -302,21 +303,21 @@ frame.on(
       numAnswersSubmitted: 10,
       answers: [
         {
-          answer: [1078, 1080, 1082, 1084, 1086, 1088, 1090],
+          answer: [1084, 1086, 1088, 1090, 1092, 1094, 1096],
           timesChosen: 8,
           percentage: 80,
           answerClass: 'similarity2',
           principle: 'Similarity'
         },
         { 
-          answer: [1092, 1094, 1096, 1098, 1100, 1102, 1104],
+          answer: [1098, 1100, 1102, 1104, 1106, 1108, 1110],
           timesChosen: 8,
           percentage: 80,
           answerClass: 'similarity2',
           principle: 'Similarity'
         },
         {
-          answer: [1078, 1080, 1082, 1084, 1086, 1088, 1090,1092, 1094, 1096, 1098, 1100, 1102, 1104],
+          answer: [1084, 1086, 1088, 1090, 1092, 1094, 1096, 1098, 1100, 1102, 1104, 1106, 1108, 1110],
           timesChosen: 2,
           percentage: 20,
           answerClass: 'hierarchy-continuity2',
@@ -535,7 +536,7 @@ frame.on(
 
     let pages = new Pages({
       pages: [
-        { page: level12 },
+        { page: level1 },
         { page: level2 },
         { page: level3 },
         { page: level4 },
@@ -1609,10 +1610,10 @@ let necklace = new Blob({
       {x: 400, y: 525},
     ],
     cords:[
-      { x: 450, y:400},
-      { x: 700, y: 150},
-      { x: 950, y:400},
-      { x:700, y:650},
+      { x: 450, y:400, id:1},
+      { x: 700, y: 150, id:2},
+      { x: 950, y:400, id:3},
+      { x:700, y:650, id:4},
     ]
   }
 
@@ -1628,14 +1629,14 @@ let necklace = new Blob({
       {x: 650, y: 525},
     ],
     cords:[
-      {x: 700, y:150},
-      { x: 870, y: 230},
-      { x: 950, y:400},
-      {x:870, y:570},
-      { x:700, y:650},
-      { x:530, y:570},
-      {x:450, y:400},
-      {x:530, y:230},
+      {x: 700, y:150, id:1},
+      { x: 870, y: 230, id:2},
+      { x: 950, y:400, id:3},
+      {x:870, y:570, id:4},
+      { x:700, y:650, id:5},
+      { x:530, y:570, id:6},
+      {x:450, y:400, id:7},
+      {x:530, y:230, id:8},
     ]
   }
  
@@ -1727,6 +1728,7 @@ currentNecklace++
 removeDots()
 nextNecklace()
 stage.update();
+selectedGroups = [];
 })
 
 // let submit = new Button({
@@ -1737,6 +1739,44 @@ stage.update();
 //   // shape.removeFrom(stage);
 //   // stage.update();
 // })
+let possibleAnswer = [[1,2], [3,4],];
+let checkAnswer = (answer, possibleAnswer)=>{
+  x = true
+  if(answer.length !== possibleAnswer.length){
+    x = false
+  }
+  for(let i = 0; i < answer.length; i++){
+    if(answer[i].length !== possibleAnswer[i].length){
+      x= false
+    }
+  }
+
+  for(let i = 0; i < answer.length; i++){
+    for(let j = 0; j < answer[i].length; j++){
+      if(answer[i][j]!= possibleAnswer[i][j]){ 
+        x= false
+      }
+    }
+  }
+return x
+}
+
+let organize2dArr=(arr)=>{
+  let x = arr.filter(arr=>{
+    if(arr[0] !== 1){
+      return arr
+    }
+  });
+
+  let y = arr.filter(arr=>{
+    if(arr[0] == 1){
+      return arr
+    }
+  });
+
+  let organized = [...y, ...x];
+  
+}
 
 document.addEventListener('keydown',(e)=>{
   if(e.key==='Enter'){
